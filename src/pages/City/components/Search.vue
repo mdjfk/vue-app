f48ed4515b561e7584a7e4178c2dbc0f2d98373c
Ivy <heybeam@163.com>
2020-05-06 21:48:04 +0800

    add city search

--- src/pages/City/components/Search.vue
+++ src/pages/City/components/Search.vue
@@ -15,13 +15,13 @@ export default {
   height: 0.72rem
   padding: 0 0.1rem
   background: $bgColor
-.input-block
-  width: 100%
-  box-sizing: border-box
-  height: 0.62rem
-  line-height: 0.62rem
-  padding: 0 0.14rem
-  text-align: center
-  border-radius: 0.1rem
-  color: #666
+  .input-block
+    width: 100%
+    box-sizing: border-box
+    height: 0.62rem
+    line-height: 0.62rem
+    padding: 0 0.14rem
+    text-align: center
+    border-radius: 0.1rem
+    color: #666
 </style>
