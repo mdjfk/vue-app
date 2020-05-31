e6dfc80fb8f836bf0219c2665845392c7cfc3fa8
Ivy <heybeam@163.com>
2020-05-31 21:24:06 +0800

    finish city css

---
+++ src/pages/City/components/Alphabet.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+
+    <li class="item">Z</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+.list
+  position: absolute
+  top: 1.58rem
+  width: 0.4rem
+  right: 0
+  bottom: 0
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-direction: column
+  .item
+    line-height: 0.42rem
+    color: $bgColor
+</style>
--- src/pages/City/components/List.vue
+++ src/pages/City/components/List.vue
@@ -1,42 +1,104 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="buttonList">
-        <div class="button-wrapper">北京</div>
-        <div class="button-wrapper">北京</div>
-        <div class="button-wrapper">北京</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="buttonList">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="buttonList">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+        </div>
+        <div class="title border-topbottom">B</div>
+        <div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+          <div class="item-list border-bottom">北京</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
 }
 </script>
 <style lang="stylus" scoped>
-.title
-  line-height: 0.44rem
-  text-indent: 0.2rem
-  background: #eee
-  color: #666
-  font-size: 0.26rem
-.buttonList
+.border-topbottom
+  &:before
+    border-color: #ccc
+  &:after
+    border-color: #ccc
+.border-bottom
+  &:before
+    border-color: #ccc
+.list
   overflow: hidden
-  padding: 0.1rem 0.6rem 0.1rem 0.1rem
-  .button-wrapper
-    float: left
-    width: 33.33%
-    border: 1px solid #ccc
-    margin: 0.1rem
-    padding: 0.1rem
-    border-radius: 0.15rem
-    text-align: center
+  position: absolute
+  top: 1.58rem
+  left: 0
+  right: 0
+  bottom: 0
+  .title
+    line-height: 0.54rem
+    padding-left: 0.2rem
+    background: #eee
+    color: #666
+    font-size: 0.26rem
+  .buttonList
+    overflow: hidden
+    padding: 0.1rem 0.6rem 0.1rem 0.1rem
+    .button-wrapper
+      float: left
+      width: 33.33%
+      .button
+        border: 1px solid #ccc
+        margin: 0.1rem
+        padding: 0.1rem
+        border-radius: 0.15rem
+        text-align: center
+  .item-list
+    line-height: 0.7rem
+    padding-left: 0.2rem
 </style>
