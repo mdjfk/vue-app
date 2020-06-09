43ddcd24c1f8b0dd80bd9fdd0974ef7c43d73c6b
Ivy <heybeam@163.com>
2020-06-09 23:20:10 +0800

    improve performance

--- src/pages/City/components/Alphabet.vue
+++ src/pages/City/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of alphabet" :key="key" @click="handleLetterClick">{{key}}</li>
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >{{item}}</li>
   </ul>
 </template>
 <script>
@@ -9,16 +18,55 @@ export default {
   props: {
     alphabet: Object
   },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.alphabet) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
     handleLetterClick (e) {
       this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].pageY - 79
+          const index = Math.floor((touchY - this.startY) / 21)
+          // console.log(index)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
 @import '~styles/variables.styl'
-
 .list
   position: absolute
   top: 1.58rem
@@ -29,7 +77,6 @@ export default {
   align-items: center
   justify-content: center
   flex-direction: column
-
   .item
     line-height: 0.42rem
     color: $bgColor
