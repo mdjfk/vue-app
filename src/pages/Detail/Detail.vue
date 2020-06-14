d930a6dec76c78311df6d0c26f6548c0739fcf64
Ivy <heybeam@163.com>
2020-06-14 21:50:29 +0800

    2020-06-14

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
     <div class="container"></div>
   </div>
 </template>
@@ -9,12 +10,27 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票'
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/Detail/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="(item,index) in list" :key="index">
+      <div class="title border-bottom">
+        <span class="title-icon"></span>
+        {{item.title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang='stylus' scoped></style>
